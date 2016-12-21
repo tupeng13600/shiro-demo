7dfd3cb22ccf106bfa85f4e696eeebbd7aebe73f
Tupeng <tupeng@gengee.cn>
2016-12-21 15:14:22 +0800

    添加部分异常处理类型

--- src/main/java/com/tp/shiro/exception/DemoExceptionHandler.java
+++ src/main/java/com/tp/shiro/exception/DemoExceptionHandler.java
@@ -3,7 +3,9 @@ package com.tp.shiro.exception;
 import com.tp.shiro.common.RespModel;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.shiro.ShiroException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,6 +25,23 @@ public class DemoExceptionHandler {
     public RespModel all(Exception e) {
         logger.debug("Exception:", e);
         return new RespModel(false).setData("系统错误，请联系相关管理人员");
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public RespModel requestTypeError(HttpRequestMethodNotSupportedException e) {
+        logger.debug("Exception:", e);
+        return new RespModel(false).setData("请求方式错误");
+    }
+
+
+    @ExceptionHandler(ShiroException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public RespModel authentication(ShiroException e) {
+        logger.debug("Exception:", e);
+        return new RespModel(false).setData("未授权或授权失败");
     }
 
     @ExceptionHandler(DemoException.class)
